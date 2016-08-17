4973215d3bef0e15d07d0aa961cc11fe7b9d9474
Lenovezhou <271130940@qq.com>
2016-08-17 16:18:28 +0800

    事件与委托的区别和使用注意

---
+++ prserve/Delegatehandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// ------------------------------------------创建委托实例------------------------------------|
+/// 现在如果你回到unity编辑器，它将显示错误消息“在类型` DelegateHandler”之外使用时，事件`DelegateHandler.buttonClickDelegate”只能出现在+ =或-=的左边。
+///            在Start方法如下改变来消除错误，别忘了在OnDisable销毁方法，否则，它将内存泄露。
+/// ---------------------------------------作者：LenoveZhou-----------------------------------|
+/// ----------------------------------------时间：16.08.17------------------------------------|
+/// </summary>
+public class Delegatehandler : MonoBehaviour {
+
+    
+    public delegate void OnButtonClickDelegate();
+    public static OnButtonClickDelegate _buttonClickDelegate;         //delegate变量声明
+    public static event OnButtonClickDelegate buttonClickDelegate;    //envet   变量申明
+    private Button button;
+    void Start() 
+    {
+        button = GameObject.FindGameObjectWithTag("button").GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClick);
+    }
+    public void OnButtonClick() 
+    {
+        buttonClickDelegate();
+    }
+    void OnDisable()
+    {
+        button.onClick.RemoveAllListeners();
+    }
+}
---
+++ prserve/objectcontrolle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// ----如果多个类监听相同的引用，这种情况是很难去跟踪的，为了避免这种情况，我们使用事件------|
+/// 它将显示错误消息“在类型` DelegateHandler”之外使用时，事件`DelegateHandler.buttonClickDelegate”
+/// 只能出现在+ =或-=的左边，要将所有的合并运算符都改成+=或-=，且在左边；
+/// ---------------------------------------作者：LenoveZhou-----------------------------------|
+/// ----------------------------------------时间：16.08.17------------------------------------|
+/// </summary>
+
+
+
+public class objectcontrolle : MonoBehaviour {
+
+    MeshRenderer objRenderer;
+    void Start() 
+    {
+        objRenderer = GetComponent<MeshRenderer>();
+        Delegatehandler.buttonClickDelegate += ChangePosition;
+        Delegatehandler.buttonClickDelegate += ChangeColor;
+        Delegatehandler.buttonClickDelegate += ChangeRotation;   //只有changerotation会执行
+    }
+    void ChangePosition() 
+    {
+        transform.position = new Vector2(transform.position.x+2f,transform.position.y);
+    }
+    void ChangeColor() 
+    {
+       
+        objRenderer.material.color = Color.yellow;
+    }
+    void ChangeRotation() 
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y+10f,transform.eulerAngles.z);
+        Debug.Log("changerotation");
+    }
+    void OnDisable() 
+    {
+        Delegatehandler.buttonClickDelegate -= ChangeColor;
+        Delegatehandler.buttonClickDelegate -= ChangePosition;
+        Delegatehandler.buttonClickDelegate -= ChangeRotation;
+
+    }
+}
